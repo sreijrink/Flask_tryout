21ed33e952da87ca5aeb5d629f61443bb9812d82
Sander Reijrink <sander@reijrink.info>
2024-07-18 22:01:24 +0200

    basic Flask code 2

--- flask_app.py
+++ flask_app.py
@@ -5,5 +5,5 @@ app=Flask(__name__)
 def home():
     return "<p>Dit is les 11 van de NHA opeldeiding - Programmeren voor beginners</p>"
 
-if __name__ == 'main__':
+if __name__ == '__main__':
     app.run(port=5000,debug=True)
